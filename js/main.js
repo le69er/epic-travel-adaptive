542a79d86c6cc70cd4b2788d6150d0642397a909
Ledger <88238674+le69er@users.noreply.github.com>
2024-12-21 13:11:19 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,18 @@
+const btn = document.querySelector('#btn-mobile-nav');
+const mobileBtnImg = document.querySelector('#mobile-nav-img');
+const mobileNav = document.querySelector('#mobile-nav');
+const header = document.querySelector('#header');
+
+
+btn.addEventListener('click', function () {
+    btn.classList.toggle('open');
+    mobileNav.classList.toggle('open');
+    header.classList.toggle('header-white-bg-mobile');
+    document.body.classList.toggle('no-scroll');
+
+    if (btn.classList.contains('open')) {
+        mobileBtnImg.src = './img/icons/close-mobile-nav.svg';
+    } else {
+        mobileBtnImg.src = './img/icons/open-mobile-nav.svg';
+    }
+})
